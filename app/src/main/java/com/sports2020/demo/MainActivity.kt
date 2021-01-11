5d431359e0b79a6cd501d44b45f8e25b26179b7c
KongXin <lgm520program@163.com>
2021-01-11 17:07:47 +0800

    状态栏

--- app/src/main/java/com/sports2020/demo/MainActivity.kt
+++ app/src/main/java/com/sports2020/demo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.sports2020.demo
 
+import android.view.KeyEvent
 import androidx.lifecycle.ViewModelProvider
 import com.sports2020.demo.databinding.ActivityMainBinding
 import com.sports2020.demo.viewmodel.ViewModelFactory
@@ -26,5 +27,22 @@ class MainActivity : BaseActivity<SplashViewModel, ActivityMainBinding>() {
 
     override fun initData() {
 
+    }
+
+    // 设置返回按钮的监听事件
+    private var exitTime: Long = 0
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        // 监听返回键，点击两次退出程序
+        if (keyCode == KeyEvent.KEYCODE_BACK && event.action == KeyEvent.ACTION_DOWN) {
+            if (System.currentTimeMillis() - exitTime > 2000) {
+                showMessageShort("再按一次退出程序")
+                exitTime = System.currentTimeMillis()
+            } else {
+                this.finish()
+            }
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
